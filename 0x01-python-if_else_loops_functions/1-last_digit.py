cb825cc0fc0cef5ee0633718520dcec24dddc0a3
Vconcept <yungaeronaut@gmail.com>
2022-09-20 14:55:20 -0700

    last digit discovery

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number[-1] > 5:
+    print("Last digit of " + {} + "is " + {} + "and is greater than 5".format(number, number[-1]
+elif number[-1] == 0:
+    print("Last digit of " + {} + "is " + {} + "and is 0".format(number, number[-1]
+elif (number[-1] < 6 and != 0):
+    print("Last digit of " + {} + "is " + {} + "and is less than 6 and not 0".format(number, number[-1]
+
