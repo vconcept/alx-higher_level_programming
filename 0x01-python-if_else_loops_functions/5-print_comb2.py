5fc8b124cb83d672f7f345d0a8e220771f782be4
Vconcept <yungaeronaut@gmail.com>
2022-09-20 15:49:24 -0700

    refix

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for numbers in range(0, 100):
     if numbers != 99:
-        print("{:2d}, ".format(numbers), end='')
+        print("{:02d}, ".format(numbers), end='')
     else:
-        print("{:2d}".format(numbers))
+        print("{:02d}".format(numbers))
