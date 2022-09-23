9912b44bbf35a83ff7561c363ff0014896b66f48
Vconcept <yungaeronaut@gmail.com>
2022-09-23 05:08:25 -0700

    fixed bugs

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,9 +5,9 @@ if __name__ == "__main__":
     if argument_no == 0:
         print("{:d} arguments.".format(argument_no))
     elif argument_no == 1:
-        print("{:d} argument".format(argument_no))
+        print("{:d} argument:".format(argument_no))
         print("{:d}: {}".format(argument_no, (sys.argv)[1]))
     else:
-        print("{:d} arguments.".format(argument_no))
+        print("{:d} arguments:".format(argument_no))
         for i in range(1, argument_no + 1):
             print("{:d}: {}".format(i, (sys.argv)[i]))
