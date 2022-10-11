be3de8e99970a3606991709565f1358d41ab812a
vconcept <yungaeronaut@gmail.com>
2022-10-11 23:53:24 +0100

    Square with error raised

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This square module generates the size of each object for us"""
+
+class Square:
+    """The major class at work here"""
+    def __init__(self, size=0): #The main attrib that specifies size
+        if not isinstance(size, int):
+            raise TypeError("size must be an Integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size #The parameter supplied becomes the size of the object
+
