36b4f5fbadf44bef8ada0993163fa73e436f5894
vconcept <yungaeronaut@gmail.com>
2022-10-11 22:59:38 +0100

    Declare square sizes

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,5 +3,6 @@ class Square:
     """This is a square class"""
 
     def __init__(self, size):
-        """This method stores the square size"""
+        """This method stores the square size.
+        """
         self.__size = size
